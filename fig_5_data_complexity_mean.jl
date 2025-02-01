1aff6fc9041a66256ee681ac4aaca8dd12f04a82
melioristic <itsmohitanand@gmail.com>
2025-02-01 12:25:25 +0100

    added data complexity distribution plot

--- fig_5_data_complexity_mean.jl
+++ fig_5_data_complexity_mean.jl
@@ -96,6 +96,10 @@ end
 var_list_beech, age_beech, lai_beech, mean_Xs_beech, std_Xs_beech, labels_beech = get_var_list("beech")
 var_list_pine, age_pine, lai_pine, mean_Xs_pine, std_Xs_pine, labels_pine = get_var_list("pine")
 
+
+age_beech
+
+
 cross_cor_beech = get_cross_cor(var_list_beech)
 cross_cor_pine = get_cross_cor(var_list_pine)
 
@@ -152,7 +156,51 @@ lai_pine_extreme = lai_pine_n[pine_ind_extreme, :]
 lai_pine_min = mean(lai_pine_extreme, dims=1) - std(lai_pine_extreme, dims=1)
 lai_pine_max = mean(lai_pine_extreme, dims=1) + std(lai_pine_extreme, dims=1)
 
+function quantile_mat(mat, q)
+    out = Vector{Float64}()
+    for i in eachindex(mat[1,:])
+        if quantile(mat[:,i], q) < 1
+            push!(out, 1)
+        else
+            push!(out, quantile(mat[:,i], q))
+        end
+    end
+    return out
+end
 
+mean_age_beech = reshape(mean_Xs_beech[:,1], 1, 101)
+std_age_beech = reshape(std_Xs_beech[:,1], 1, 101)
+
+mean_lai_beech = reshape(mean_Xs_beech[:,2], 1, 101)
+std_lai_beech = reshape(std_Xs_beech[:,2], 1, 101)
+
+mean_age_pine = reshape(mean_Xs_pine[:,1], 1, 101)
+std_age_pine = reshape(std_Xs_pine[:,1], 1, 101)
+
+mean_lai_pine = reshape(mean_Xs_pine[:,2], 1, 101)
+std_lai_pine = reshape(std_Xs_pine[:,2], 1, 101)
+
+
+age_beech_ori = age_beech .*std_age_beech .+ mean_age_beech
+
+median_age_beech = quantile_mat(age_beech_ori, 0.5)
+q95_age_beech = quantile_mat(age_beech_ori, 0.95)
+q05_age_beech = quantile_mat(age_beech_ori, 0.05)
+
+lai_beech_ori = lai_beech .*std_lai_beech .+ mean_lai_beech
+median_lai_beech = quantile_mat(lai_beech_ori, 0.5)
+q95_lai_beech = quantile_mat(lai_beech_ori, 0.95)
+q05_lai_beech = quantile_mat(lai_beech_ori, 0.05)
+
+age_pine_ori = age_pine .*std_age_pine .+ mean_age_pine
+median_age_pine = quantile_mat(age_pine_ori, 0.5)
+q95_age_pine = quantile_mat(age_pine_ori, 0.95)
+q05_age_pine = quantile_mat(age_pine_ori, 0.05)
+
+lai_pine_ori = lai_pine .*std_lai_pine .+ mean_lai_pine
+median_lai_pine = quantile_mat(lai_pine_ori, 0.5)
+q95_lai_pine = quantile_mat(lai_pine_ori, 0.95)
+q05_lai_pine = quantile_mat(lai_pine_ori, 0.05)
 
 
 ### Figure starts here
@@ -165,22 +213,67 @@ textheme = Theme(fonts=(; regular=texfont(:text),
 
 set_theme!(textheme)
 
-f = Figure(size = (1200,800))
+##################
+##### FIGURE #####
+##################
 
-ax_top = f[3,1] = GridLayout()
-ax_bottom = f[1:2,1] = GridLayout()
+f = Figure(size = (1200,1000))
 
-# ax_climate = Axis(ax_top[1,1], 
+ax_dist = f[1,1] = GridLayout()
+
+ax_corr = f[2,1] = GridLayout()
+
+ax_composite = f[3:4,1] = GridLayout()
+
+# ax_climate = Axis(ax_composite[1,1], 
 #     xgridvisible=false, 
 #     ygridvisible=false,
 #     yticks = ([1:11;], xtick_corr_all),
 #     xticks = ([1:11;], xtick_corr_all),
 #     xticklabelrotation = pi/3,
 #     )
+f
 
-ax_age_beech_cor = Axis(ax_top[1,1], 
-    xlabel="Age", 
-    ylabel="Age (Beech) ", 
+
+ax_age_beech_dist = Axis(ax_dist[1,1], 
+    xlabel="Age (Beech)", 
+    ylabel="Density", 
+    xgridvisible=false, 
+    ygridvisible=false,
+    yscale = log10,
+     
+    )
+
+ax_lai_beech_dist = Axis(ax_dist[1,2],
+    xlabel="LAI (Beech)", 
+    ylabel="Density", 
+    xgridvisible=false, 
+    ygridvisible=false,
+    yscale = log10,
+
+    )
+
+ax_age_pine_dist = Axis(ax_dist[1,3], 
+    xlabel="Age (Pine)", 
+    ylabel="Density", 
+    xgridvisible=false, 
+    ygridvisible=false,
+    yscale = log10,
+
+    )
+
+ax_lai_pine_dist = Axis(ax_dist[1,4],
+    xlabel="LAI (Pine)", 
+    ylabel="Density", 
+    xgridvisible=false, 
+    ygridvisible=false,
+    yscale = log10,
+
+    )
+
+ax_age_beech_cor = Axis(ax_corr[1,1], 
+    xlabel="Age (Beech)", 
+    ylabel="Age", 
     xgridvisible=false, 
     ygridvisible=false,
     xticks = end_age_beech_ticklabels,
@@ -188,30 +281,29 @@ ax_age_beech_cor = Axis(ax_top[1,1],
     yticks = end_age_beech_ticklabels,
     )
 
-ax_lai_beech_cor = Axis(ax_top[1,2], 
-    xlabel="LAI", 
-    ylabel="LAI (Beech)", 
+ax_lai_beech_cor = Axis(ax_corr[1,2], 
+    xlabel="LAI (Beech)", 
+    ylabel="LAI", 
     xgridvisible=false, 
     ygridvisible=false,
     xticks = end_lai_beech_ticklabels,
     xticklabelrotation = pi/3,
     yticks = end_lai_beech_ticklabels,
     )
-ax_age_pine_cor = Axis(ax_top[1,3], 
-    xlabel="Age", 
-    ylabel="Age (Pine)", 
+ax_age_pine_cor = Axis(ax_corr[1,3], 
+    xlabel="Age (Pine)", 
+    ylabel="Age", 
     xgridvisible=false, 
     ygridvisible=false, 
     xticks = end_age_pine_ticklabels,
     xticklabelrotation = pi/3,
-    yticks = end_age_pine_ticklabels,
-    aspect = AxisAspect(1),)
+    yticks = end_age_pine_ticklabels,)
 
 
 
-ax_lai_pine_cor = Axis(ax_top[1,4], 
-    xlabel="LAI", 
-    ylabel="LAI (Pine)", 
+ax_lai_pine_cor = Axis(ax_corr[1,4], 
+    xlabel="LAI (Pine)", 
+    ylabel="LAI", 
     xgridvisible=false, 
     ygridvisible=false,
     xticks = end_lai_pine_ticklabels,
@@ -222,46 +314,62 @@ ax_lai_pine_cor = Axis(ax_top[1,4],
 
 f
 
-ax_age_beech = Axis(ax_bottom[1,1:5], 
+ax_age_beech = Axis(ax_composite[1,1:5], 
             xlabel="Age", 
             ylabel="Count Anomaly", 
             xgridvisible=false, 
             ygridvisible=false,
             )
 
-ax_lai_beech = Axis(ax_bottom[2,1:5], 
+ax_lai_beech = Axis(ax_composite[2,1:5], 
             xlabel="LAI", 
             ylabel="Count Anomaly", 
             xgridvisible=false, 
             ygridvisible=false)
 
-ax_age_pine = Axis(ax_bottom[1,6:10], 
+ax_age_pine = Axis(ax_composite[1,6:10], 
     xlabel="Age", 
     ylabel="Count Anomaly", 
     xgridvisible=false, 
     ygridvisible=false)
 
 
-ax_lai_pine = Axis(ax_bottom[2,6:10], 
+ax_lai_pine = Axis(ax_composite[2,6:10], 
     xlabel="LAI", 
     ylabel="Count Anomaly", 
     xgridvisible=false, 
     ygridvisible=false)
 
-
+f
 
 
 # f
-# colorrange = (-0.75, 0.75)
-# hm_climate = heatmap!(ax_climate, cross_cor_beech, colormap=(:balance, 1), colorrange = colorrange)
-# ax_climate.yreversed = true
-# Colorbar(ax_top[1, 5], hm_climate, vertical=true)
-# f
+
+q05_age_beech
+
+lines!(ax_age_beech_dist,beech_bins[1,1:end_age_beech], median_age_beech[1:end_age_beech], color = p["orange"], linewidth = 2)
+band!(ax_age_beech_dist,  beech_bins[1,1:end_age_beech], q05_age_beech[1:end_age_beech], q95_age_beech[1:end_age_beech], color = (p["orange"], 0.25))
+
+lines!(ax_lai_beech_dist, beech_bins[2,1:end_lai_beech], median_lai_beech[1:end_lai_beech], color = p["green"], linewidth = 2)
+band!(ax_lai_beech_dist, beech_bins[2,1:end_lai_beech], q05_lai_beech[1:end_lai_beech], q95_lai_beech[1:end_lai_beech], color = (p["green"], 0.25))
+
+lines!(ax_age_pine_dist, pine_bins[1,1:end_age_pine], median_age_pine[1:end_age_pine], color = p["orange"], linewidth = 2)
+band!(ax_age_pine_dist, pine_bins[1,1:end_age_pine], q05_age_pine[1:end_age_pine], q95_age_pine[1:end_age_pine], color = (p["orange"], 0.25))
+
+lines!(ax_lai_pine_dist, pine_bins[2,1:end_lai_pine], median_lai_pine[1:end_lai_pine], color = p["green"], linewidth = 2)
+band!(ax_lai_pine_dist, pine_bins[2,1:end_lai_pine], q05_lai_pine[1:end_lai_pine], q95_lai_pine[1:end_lai_pine], color = (p["green"], 0.25))
+
+f
+
+colorrange = (-0.75, 0.75)
+
 
 heatmap!(ax_age_beech_cor, cross_cor_age_beech, colormap=(:balance, 1), colorrange = colorrange)
 heatmap!(ax_age_pine_cor, cross_cor_age_pine, colormap=(:balance, 1), colorrange = colorrange)
 heatmap!(ax_lai_beech_cor, cross_cor_lai_beech, colormap=(:balance, 1), colorrange = colorrange)
-heatmap!(ax_lai_pine_cor, cross_cor_lai_pine, colormap=(:balance, 1), colorrange = colorrange)
+hm = heatmap!(ax_lai_pine_cor, cross_cor_lai_pine, colormap=(:balance, 1), colorrange = colorrange)
+Colorbar(ax_corr[1,5], hm, vertical=true, label="Correlation")
+f
 f
 ax_age_beech_cor.yreversed = true
 ax_age_pine_cor.yreversed = true
@@ -304,9 +412,9 @@ end
 
 f
 
-label = ["c", "d", "g", "h"]
+label = ["a", "b", "c", "d"]
 for i=1:4
-    Label(ax_top[1,i, TopLeft()], 
+    Label(ax_dist[1,i, TopLeft()], 
         label[i],
         fontsize=16, 
         font = :bold,
@@ -316,10 +424,22 @@ end
 f
 
 
-label = ["a", "e", "b", "f"]
+label = ["e", "f", "g", "h"]
+for i=1:4
+    Label(ax_corr[1,i, TopLeft()], 
+        label[i],
+        fontsize=16, 
+        font = :bold,
+        halign = :right, 
+        padding = (10,30,5,0) )
+end
+f
+
+
+label = ["i", "k", "j", "l"]
 
 for (i, ind) in enumerate([(1,1:5), (1,6:10), (2,1:5), (2,6:10)])
-    Label(ax_bottom[ind[1], ind[2], TopLeft()], 
+    Label(ax_composite[ind[1], ind[2], TopLeft()], 
         label[i],
         fontsize=16, 
         font = :bold,
@@ -330,10 +450,16 @@ end
 f
 
 
-ax_age_beech_cor.aspect = 1
-ax_age_pine_cor.aspect = 1
-ax_lai_beech_cor.aspect = 1
-ax_lai_pine_cor.aspect = 1
+# ax_age_beech_cor.aspect = 1
+# ax_age_pine_cor.aspect = 1
+# ax_lai_beech_cor.aspect = 1
+# ax_lai_pine_cor.aspect = 1
+
+# ax_age_beech_dist.aspect = 1
+# ax_lai_beech_dist.aspect = 1
+# ax_age_pine_dist.aspect = 1
+# ax_lai_pine_dist.aspect = 1
+
 f
 lim = 2.5
 ylims!(ax_age_beech, -lim, lim)
@@ -341,7 +467,6 @@ ylims!(ax_age_pine, -lim, lim)
 ylims!(ax_lai_beech, -lim, lim)
 ylims!(ax_lai_pine, -lim, lim)
 
-f
 hidespines!(ax_age_beech_cor, :t, :r)
 hidespines!(ax_age_pine_cor, :t, :r)
 hidespines!(ax_lai_beech_cor, :t, :r)
@@ -355,20 +480,23 @@ hidespines!(ax_lai_pine, :t, :r)
 f
 save("images/fig_5_data_complexity_mean.pdf", f)
 
-f = Figure(size = (600,600))
-climate_ax = Axis(f[1,1], 
-    xgridvisible=false, 
-    ygridvisible=false,
-    yticks = ([1:11;], xtick_corr_all),
-    xticks = ([1:11;], xtick_corr_all),
-    xticklabelrotation = pi/3,
-    )
 
-hm_climate = heatmap!(climate_ax, cross_cor_beech, colormap=(:balance, 1), colorrange = colorrange)
-climate_ax.yreversed = true
-Colorbar(f[1,2], hm_climate, vertical=true)
-hidespines!(climate_ax, :t, :r)
-climate_ax.aspect = 1
-f
 
-save("images/fig_a1_climate.pdf", f)
+
+# f = Figure(size = (600,600))
+# climate_ax = Axis(f[1,1], 
+#     xgridvisible=false, 
+#     ygridvisible=false,
+#     yticks = ([1:11;], xtick_corr_all),
+#     xticks = ([1:11;], xtick_corr_all),
+#     xticklabelrotation = pi/3,
+#     )
+
+# hm_climate = heatmap!(climate_ax, cross_cor_beech, colormap=(:balance, 1), colorrange = colorrange)
+# climate_ax.yreversed = true
+# Colorbar(f[1,2], hm_climate, vertical=true)
+# hidespines!(climate_ax, :t, :r)
+# climate_ax.aspect = 1
+# f
+
+# save("images/fig_a1_climate.pdf", f)
